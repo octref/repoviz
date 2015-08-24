7d64a8473f8b417c706fe38d23375e38e11b44d5
octref <octref@gmail.com>
2015-08-24 08:56:27 -0400

    Finish stargazer

--- data/stargazer.js
+++ data/stargazer.js
@@ -7,7 +7,16 @@ var qs = require('query-string'),
     _ = require('lodash'),
     async = require('async');
 
-var stars = {};
+var stars = [];
+
+function addStars(body) {
+  _.forEach(JSON.parse(body), function(star) {
+    stars.push({
+      user: star.user.login,
+      time: star.starred_at
+    });
+  });
+}
 
 module.exports = function stargazer(repoURL, cb) {
   var options = {
@@ -17,35 +26,46 @@ module.exports = function stargazer(repoURL, cb) {
   };
 
   req(options, function(err, res, body) {
-    var links = parseLink(res.headers.link);
-    var lastPage = parseInt(qs.parse(links.last).page, 10);
-    var pages = _.range(1, lastPage + 1);
+    // Multiple pages
+    if (res.headers.link) {
+      var links = parseLink(res.headers.link);
+      var lastPage = parseInt(qs.parse(links.last).page, 10);
+      var pages = _.range(1, lastPage + 1);
 
-    var iterator = function(page, done) {
-      var options = {
-        url: '/repos/' + repoURL + '/stargazers',
-        headers: { 'Accept': 'application/vnd.github.v3.star+json' },
-        qs: { 'per_page': 100, page: page }
+      var iterator = function(page, done) {
+        var options = {
+          url: '/repos/' + repoURL + '/stargazers',
+          headers: { 'Accept': 'application/vnd.github.v3.star+json' },
+          qs: { 'per_page': 100, page: page }
+        };
+
+        req(options, function(err, res, body) {
+          addStars(body);
+          console.log('Done with page ' + page);
+          console.log('Total star length: ' + stars.length);
+          done();
+        });
       };
 
-      req(options, function(err, res, body) {
-        _.forEach(JSON.parse(body), function(star) {
-          if (!star.user) {
-            console.log('error');
-            console.log(JSON.parse(body));
-            console.log(star);
-          }
-          stars[star.user.login] = star.starred_at;
-        });
-        console.log('Done with page ' + page);
-        console.log('Total star length: ' + Object.keys(stars).length);
-        done();
-      });
-    };
+      async.eachLimit(pages, 3, iterator, function(err) {
+        console.log('All done');
 
-    async.eachLimit(pages, 3, iterator, function(err) {
-      console.log('All done');
-      cb(stars);
-    });
+        var sorted = _.sortBy(stars, function(star) {
+          return star.time;
+        });
+
+        if (cb) cb(sorted);
+      });
+    }
+    // Single page
+    else {
+      addStars(body);
+
+      var sorted = _.sortBy(stars, function(star) {
+        return star.time;
+      });
+
+      if (cb) cb(sorted);
+    }
   });
 };
